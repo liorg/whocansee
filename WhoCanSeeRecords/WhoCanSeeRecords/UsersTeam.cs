25cb2a007cb403fc20bca0c5148d9791c98fa1ec
lior grossman <lior_g@g-s.co.il>
2015-09-03 13:59:46 +0300

    add

--- WhoCanSeeRecords/WhoCanSeeRecords/UsersTeam.cs
+++ WhoCanSeeRecords/WhoCanSeeRecords/UsersTeam.cs
@@ -7,91 +7,47 @@ using System.Threading.Tasks;
 
 namespace WhoCanSeeRecords
 {
-   public class UsersTeam
+    public class UsersTeam
+    {
+        static UsersTeam _usersTeam = null;
+        static object _lock = new object();
+        List<Guid> _usersPremission = null;
 
-  {
+        public List<Guid> UsersPremission
+        {
+            get
+            {
+                return _usersPremission;
+            }
+        }
 
-       static UsersTeam _usersTeam=null;
+        protected UsersTeam()
+        {
+            Init();
+        }
 
-       static object _lock=new object();
+        void Init()
+        {
+            if (_usersPremission == null)
+            {
+                _usersPremission = new List<Guid>();     //users that's on specific team!!!!
+                _usersPremission.Add(new Guid("{2CDE69CD-DA28-5555-7777-4022507DD859}"));    //SOME USER    can see
+                _usersPremission.Add(new Guid("{2CDE69CD-DA28-4444-8888-4022507DD859}"));    //   SOME USER
+                //{2CDE69CD-DA28-2222-6666-4022507DD859} user who can not see
+            }
+        }
 
-       List<Guid>              _usersPremission        =null;
-
-       public List<Guid> UsersPremission
-
-       {
-
-         get
-
-         {
-
-              return _usersPremission;
-
-         }
-
-       }
-
- 
-
- 
-
-       protected UsersTeam()
-
-       {
-
-         Init();
-
-       }
-
- 
-
-       void Init()
-
-       {
-
-         if(_usersPremission == null)
-
-         {
-
-              _usersPremission = new List<Guid>();     //users that's on specific team!!!!
-
-              _usersPremission.Add(new Guid("{2CDE69CD-DA28-5555-7777-4022507DD859}"));    //SOME USER    can see
-
-              _usersPremission.Add(new Guid("{2CDE69CD-DA28-4444-8888-4022507DD859}"));    //   SOME USER
-
-              //{6e13a10f-7bd3-e311-93fc-005056a814c8} mentadev4 who can not see
-         }
-       }
-
-       public static UsersTeam GetSinglton(IOrganizationService service, ConfigCaching config)
-       {
-         if(_usersTeam == null)
-
-         {
-
-              lock(_lock)
-
-              {
-
-                if(_usersTeam == null)
-
+        public static UsersTeam GetSinglton(IOrganizationService service, ConfigCaching config)
+        {
+            if (_usersTeam == null)
+            {
+                lock (_lock)
                 {
-
-                     _usersTeam = new UsersTeam();
-
+                    if (_usersTeam == null)
+                        _usersTeam = new UsersTeam();
                 }
-
-              }
-
-         }
-
-         return _usersTeam;
-
-       }
-
- 
-
-  }
-
-
+            }
+            return _usersTeam;
+        }
+    }
 }
